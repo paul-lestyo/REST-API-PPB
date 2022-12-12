85e854266bb99d7cebe491728427d35fd791a097
paul_lestyo <paulus.lestyo@student.uns.ac.id>
2022-12-13 00:22:05 +0700

    add auth

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express')
 const cors = require('cors')
 const { UserRoute } = require('./routers/UserRoute.js')
 const { CategoryRoute } = require('./routers/CategoryRoute.js')
+const { AuthRoute } = require('./routers/AuthRoute.js')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
 app.use(UserRoute)
 app.use(CategoryRoute)
+app.use(AuthRoute)
 
 app.listen(5000, () => console.log('Server up and running....'))
 
