f78616b479cbf7c013bfb66f4939d1793ab58a9d
paul_lestyo <paulus.lestyo@student.uns.ac.id>
2022-12-19 17:03:56 +0700

    update feature search product

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,8 +1,19 @@
+const { Sequelize } = require('sequelize');
 const {Product} = require('../models/ProductModel.js')
+const Op = Sequelize.Op;
 
 const geProducts = async(req, res) => {
 	try {
-		const response = await Product.findAll()
+		let response;
+		if(req.query.search != null) {
+			response = await Product.findAll({
+				where: {
+					name: {[Op.like]: `%${req.query.search}%`}
+				}
+			})
+		} else {
+			response = await Product.findAll()
+		}
 		res.status(200).json(response)
 	} catch (error) {
 		console.log(error.message)
